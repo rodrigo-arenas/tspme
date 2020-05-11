018bc392e947fb4de3001c02af3222933f226645
rodrigoarenas <rodrigo.arenas456@outlook.com>
2020-05-10 23:19:37 -0500

    Added default parameters in SimulatedAnnealing class

--- tspme/Metaheuristics.py
+++ tspme/Metaheuristics.py
@@ -1,4 +1,7 @@
 class SimulatedAnnealing:
 
-    def __init__(self):
-        pass
+    def __init__(self, init_temp=0.05, alpha=0.995, stop_temp=0.5, n_cycles=100):
+        self.init_temp = init_temp
+        self.alpha = alpha
+        self.stop_temp = stop_temp
+        self.n_cycles = n_cycles
