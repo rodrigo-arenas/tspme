c84ea30649ea439b0563dbe55a9085cd421307c2
rodrigoarenas <rodrigo.arenas456@outlook.com>
2020-05-11 20:44:29 -0500

    Added distance_matrix method to calculate euclidian distance of locations_generator

--- tspme/Metaheuristics.py
+++ tspme/Metaheuristics.py
@@ -1,3 +1,6 @@
+from sklearn.metrics.pairwise import euclidean_distances
+
+
 class SimulatedAnnealing:
 
     def __init__(self, init_temp=0.05, alpha=0.995, stop_temp=0.5, n_cycles=100):
@@ -5,3 +8,15 @@ class SimulatedAnnealing:
         self.alpha = alpha
         self.stop_temp = stop_temp
         self.n_cycles = n_cycles
+        self.x = None
+        self.y = None
+        self.cost_matrix = None
+        self.locations_generator = None
+
+    def distance_matrix(self, locations_generator):
+        self.locations_generator = locations_generator
+        self.cost_matrix = euclidean_distances(self.locations_generator["locations"])
+        return self.cost_matrix
+
+
+
