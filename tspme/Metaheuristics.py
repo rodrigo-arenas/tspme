e1b7537d42fdf400aade6b799060b06facb31e9f
rodrigoarenas <rodrigo.arenas456@outlook.com>
2020-05-11 23:10:19 -0500

    Added 2-OPT interchange method

--- tspme/Metaheuristics.py
+++ tspme/Metaheuristics.py
@@ -1,5 +1,7 @@
 import numpy as np
 import random
+import functools
+import operator
 from sklearn.metrics.pairwise import euclidean_distances
 from tspme.utils.CustomerProperties import LazyProperty
 
@@ -27,6 +29,15 @@ class SimulatedAnnealing:
 
     def tour_len(self, tour):
         return np.sum(self.cost_matrix[tour[i]][tour[i+1]] for i in range(self.size-1))
+
+    def opt2(self, route):
+        new_route = route.copy()
+        _init_pos = random.sample(range(self.size-2), 1)[0]
+        _end_pos = random.sample(range(_init_pos+1, self.size-1), 1)[0]
+        new_route = [new_route[:_init_pos], new_route[_end_pos:_init_pos:-1], new_route[_end_pos:]]
+        return np.hstack(new_route).astype(int)
+
+
 
 
 
