4a62900efed87ffff814ce6ccf48d1632fb396e4
rodrigoarenas <rodrigo.arenas456@outlook.com>
2020-05-12 00:05:31 -0500

    Added full fitting example

--- tspme/demo/ten_cities_demo.py
+++ tspme/demo/ten_cities_demo.py
@@ -3,14 +3,13 @@ import numpy as np
 from tspme.utils.RoutesGenerator import RandomRouteGenerator
 from tspme.Metaheuristics import SimulatedAnnealing
 
-SIZE = 5
+SIZE = 10
 route_generator = RandomRouteGenerator(size=SIZE)
 routes = route_generator.generate()
 
 sa = SimulatedAnnealing()
 sa.distance_matrix(routes)
-print(routes)
-initial_solution = random.sample(range(SIZE), SIZE)
-print(sa.size)
-print(initial_solution)
+solution = sa.fit()
+print(solution)
+
 
