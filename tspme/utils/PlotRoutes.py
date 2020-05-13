7a49bdb4fa7fbdb49918ecc2b4355ff6f4e68a80
rodrigoarenas <rodrigo.arenas456@outlook.com>
2020-05-12 21:44:58 -0500

    Added plot_routes function Removed TSPlot and became functions

--- tspme/utils/PlotRoutes.py
+++ tspme/utils/PlotRoutes.py
@@ -1,16 +1,18 @@
 import matplotlib.pyplot as plt
 
 
-class TSPlot:
-    """
-    Plotting class for TSP solutions and cities selection
-    """
-    def __init__(self):
-        pass
+def plot_cities(cities: dict = None, figsize=(8, 6), **kwargs):
+    fig = plt.figure(figsize=figsize)
+    plt.scatter(cities['x'], cities['y'], c='black', alpha=0.5, **kwargs)
+    return fig
 
-    @staticmethod
-    def plot_cities(cities: dict = None, figsize=(8, 6), **kwargs):
-        fig = plt.figure(figsize=figsize)
-        plt.scatter(cities['x'], cities['y'], c='black', alpha=0.5, **kwargs)
-        return fig
+
+def plot_routes(cities: dict = None, solution: dict = None, figsize=(8, 6), **kwargs):
+    fig = plt.figure(figsize=figsize)
+    index = solution['route']
+    x = cities['x'][index]
+    y = cities['y'][index]
+    plt.plot(x, y, '-o', c='black', alpha=0.5, **kwargs)
+    plt.title('Final Solution Route')
+    return fig
 
