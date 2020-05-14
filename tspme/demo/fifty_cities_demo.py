3bb2e82542ef38a2e580a76a688d3bf0842256a7
rodrigoarenas <rodrigo.arenas456@outlook.com>
2020-05-13 22:04:44 -0500

    Added plot_history example

--- tspme/demo/fifty_cities_demo.py
+++ tspme/demo/fifty_cities_demo.py
@@ -1,18 +1,19 @@
 import matplotlib.pyplot as plt
 from tspme.utils.routes_generator import RandomRouteGenerator
-from tspme.utils.plot_routes import plot_routes
+from tspme.utils.plot_routes import plot_routes, plot_history
 from tspme.metaheuristics import SimulatedAnnealing
 
 
-SIZE = 50
+SIZE = 100
 route_generator = RandomRouteGenerator(size=SIZE)
 routes = route_generator.generate()
 
 sa = SimulatedAnnealing()
 sa.set_distance_matrix(routes)
-solution = sa.fit(return_cost_hist=False)
+solution = sa.fit(return_cost_hist=True)
 print(solution)
 plot_routes(cities=routes, solution=solution)
+plot_history(solution=solution)
 plt.show()
 
 
